90b09d3f1167fdbabbbd6c86a871f3d7ea1e8070
franckentien <franckentien@hotmail.fr>
2018-03-15 17:16:53 +0100

    Complete Convention Admin Page

--- src/ConventionGeek/EventBundle/Admin/ConventionAdmin.php
+++ src/ConventionGeek/EventBundle/Admin/ConventionAdmin.php
@@ -3,26 +3,61 @@
 // src/Admin/ContactAdmin.php
 namespace ConventionGeek\EventBundle\Admin;
 
+
 use Sonata\AdminBundle\Admin\AbstractAdmin;
 use Sonata\AdminBundle\Datagrid\ListMapper;
 use Sonata\AdminBundle\Datagrid\DatagridMapper;
 use Sonata\AdminBundle\Form\FormMapper;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Sonata\AdminBundle\Form\Type\ModelAutocompleteType;
+use Sonata\AdminBundle\Form\Type\ModelListType;
+
 
 class ConventionAdmin extends AbstractAdmin
 {
     protected function configureFormFields(FormMapper $formMapper)
     {
-        $formMapper->add('id', TextType::class);
+        $formMapper->add('id')
+            ->add('nom')
+            ->add('site')
+            ->add('facebook')
+            ->add('twitter')
+            ->add('lieu')
+            //->add('departement', 'sonata_type_collection')
+            ->add('meta')
+            ->add('description')
+            ->add('actif')
+            ->add('email')
+        ;
     }
 
     protected function configureDatagridFilters(DatagridMapper $datagridMapper)
     {
-        $datagridMapper->add('id');
+        $datagridMapper->add('id')
+            ->add('nom')
+            ->add('site')
+            ->add('facebook')
+            ->add('twitter')
+            ->add('lieu')
+            //->add('departement')
+            ->add('meta')
+            ->add('description')
+            ->add('actif')
+            ->add('email');
     }
 
     protected function configureListFields(ListMapper $listMapper)
     {
-        $listMapper->addIdentifier('id');
+        $listMapper->addIdentifier('id')
+            ->add('nom')
+            ->add('site')
+            ->add('facebook')
+            ->add('twitter')
+            ->add('lieu')
+            //->add('departement')
+            ->add('meta')
+            ->add('description')
+            ->add('actif')
+            ->add('email');
     }
 }
+
