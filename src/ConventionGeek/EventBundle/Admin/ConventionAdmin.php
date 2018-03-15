d84c4c1312bd6848432e7959ac7cffc12f4a9799
franckentien <franckentien@hotmail.fr>
2018-03-15 16:27:25 +0100

    Add Contact to Admin Page

---
+++ src/ConventionGeek/EventBundle/Admin/ConventionAdmin.php
@@ -0,0 +1,28 @@
+<?php
+
+// src/Admin/ContactAdmin.php
+namespace ConventionGeek\EventBundle\Admin;
+
+use Sonata\AdminBundle\Admin\AbstractAdmin;
+use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Datagrid\DatagridMapper;
+use Sonata\AdminBundle\Form\FormMapper;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+
+class ConventionAdmin extends AbstractAdmin
+{
+    protected function configureFormFields(FormMapper $formMapper)
+    {
+        $formMapper->add('id', TextType::class);
+    }
+
+    protected function configureDatagridFilters(DatagridMapper $datagridMapper)
+    {
+        $datagridMapper->add('id');
+    }
+
+    protected function configureListFields(ListMapper $listMapper)
+    {
+        $listMapper->addIdentifier('id');
+    }
+}
--- src/ConventionGeek/EventBundle/ConventionGeekEventBundle.php
+++ src/ConventionGeek/EventBundle/ConventionGeekEventBundle.php
@@ -4,6 +4,9 @@ namespace ConventionGeek\EventBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
+use Sonata\AdminBundle\Admin\AbstractAdmin;
+use Sonata\AdminBundle\Route\RouteCollection;
+
 class ConventionGeekEventBundle extends Bundle
 {
 
