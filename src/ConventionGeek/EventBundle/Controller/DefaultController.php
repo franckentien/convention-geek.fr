bdbd07ca35d0505b50b5a00aa8172e5a2c5672a7
franckentien <franckentien@hotmail.fr>
2018-03-17 18:52:53 +0100

    Gestion basique de la date copier le script android

--- src/ConventionGeek/EventBundle/Controller/DefaultController.php
+++ src/ConventionGeek/EventBundle/Controller/DefaultController.php
@@ -6,8 +6,50 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DefaultController extends Controller
 {
+
+
+
+    private function getDisplayDate($DateDebut, $DateFin){
+
+        //TODO Copie Android Script
+        $jourDebut = date_format($DateDebut, 'd') ;
+        $jourFin = date_format($DateFin, 'd');
+        return $jourDebut . '-' .$jourFin;
+    }
+
+
+    private function getEvent(){
+
+        $repository = $this
+            ->getDoctrine()
+            ->getManager()
+            ->getRepository('ConventionGeekEventBundle:DateEvent')
+        ;
+
+        $listDateEvents = $repository->findAll();
+
+        $listEvent = array();
+
+        foreach ($listDateEvents as $event) {
+
+            $date = $this->getDisplayDate($event->getDateDebut(), $event->getDateFin());
+
+            array_push($listEvent, array(
+                'date'   => $date,
+                'name' => $event->getEvenement(),
+                'place'  => 'place',
+                'informateur' => 'informateur'));
+        }
+
+        return $listEvent;
+    }
+
     public function indexAction()
     {
-        return $this->render('default/index.html.twig', []);
+
+        $listEvent = $this->getEvent();
+
+
+        return $this->render('default/index.html.twig', array('listEvenement' => $listEvent));
     }
 }
