f6acdb129107f2c4beae430539c47236c4b66895
franckentien <franckentien@hotmail.fr>
2018-08-12 16:43:54 +0200

    Rajout du formulaire contact

--- src/ConventionGeek/ContactBundle/Controller/DefaultController.php
+++ src/ConventionGeek/ContactBundle/Controller/DefaultController.php
@@ -3,23 +3,28 @@
 namespace ConventionGeek\ContactBundle\Controller;
 
 use ConventionGeek\ContactBundle\Entity\Contact;
-use ConventionGeek\ContactBundle\Form\ContactType;
-use ConventionGeek\EventBundle\Entity\Convention;
-use ConventionGeek\EventBundle\Entity\DateEvent;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use ConventionGeek\ContactBundle\Form\Type\ContactType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller
 {
 
-    public function formulairesAction(){
+    public function formulairesAction(Request $request){
 
         $contact = new Contact();
-        //$formContact   = $this->get('form.factory')->create(ContactType::class, $contact);
+        $formContact   = $this->get('form.factory')->create(ContactType::class, $contact);
 
 
-        return $this->render('@ConventionGeekContact/formulaires/formulaires.twig');
+        if ($request->isMethod('POST') && $formContact->handleRequest($request)->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($contact);
+            $em->flush();
+
+            return $this->render('@ConventionGeekContact/formulaires/formulaires.twig', array('form' => $formContact->createView(), 'toast' => true));
+        }
+
+        return $this->render('@ConventionGeekContact/formulaires/formulaires.twig', array('form' => $formContact->createView(), 'toast' => false) );
     }
 
 }
