41dff55e26c2eea55d60a196ad991687fd39c2aa
franckentien <franckentien@hotmail.fr>
2018-08-16 21:37:41 +0200

    Rajout du top app bar normal

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,20 +4,17 @@ console.log('hello world 1');
 import {MDCRipple} from '@material/ripple';
 
 
+import {MDCTopAppBar} from '@material/top-app-bar/index';
 
-// import {MDCTemporaryDrawer} from '@material/drawer';
-// const drawer = new MDCPersistentDrawer(document.querySelector('.mdc-drawer--persistent'));
-// document.querySelector('.demo-menu').addEventListener('click', () => drawer.open = !drawer.open);
+// Instantiation
+const topAppBarElement = document.querySelector('.mdc-top-app-bar');
+const topAppBar = new MDCTopAppBar(topAppBarElement);
 
 import {MDCTemporaryDrawer} from '@material/drawer';
 var drawer = new MDCTemporaryDrawer(document.querySelector('.mdc-drawer--temporary'));
 
 document.querySelector('.demo-menu').addEventListener('click', function() { drawer.open = true;});
-// drawerEl.addEventListener('MDCTemporaryDrawer:open', function() {
-//     console.log('Received MDCTemporaryDrawer:open');
-// });
-// drawerEl.addEventListener('MDCTemporaryDrawer:close', function() {
-//     console.log('Received MDCTemporaryDrawer:close');
-// });
+
+
 
 console.log('hello world 2');
