4ed6a838ddd3f8c421c1edda4bc17cb9f7c0a42b
franckentien <franckentien@hotmail.fr>
2018-08-25 20:49:58 +0200

    WIP layout

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,25 +1,52 @@
 require('../css/app.scss');
 console.log('hello world 1');
 
+import {MDCTemporaryDrawer} from '@material/drawer';
+import {MDCDialog} from '@material/dialog';
+import {MDCTabBar} from '@material/tab-bar';
+import {MDCSnackbar} from '@material/snackbar';
 
 //MDCTemporaryDrawer
-import {MDCTemporaryDrawer} from '@material/drawer';
+
 var drawer = new MDCTemporaryDrawer(document.querySelector('.mdc-drawer--temporary'));
 document.querySelector('.demo-menu').addEventListener('click', function() { drawer.open = true;});
 
 //MDCTabBar
-import {MDCTabBar} from '@material/tab-bar';
 const tabBar = new MDCTabBar(document.querySelector('.mdc-tab-bar'));
 
 //In each Page
 //tabBar.activateTab(2);
 
-import {MDCFormField} from '@material/form-field';
-import {MDCCheckbox} from '@material/checkbox';
+var dialogDom = document.querySelector('#remove-account-dialog');
 
-const checkbox = new MDCCheckbox(document.querySelector('.mdc-checkbox'));
-const formField = new MDCFormField(document.querySelector('.mdc-form-field'));
-formField.input = checkbox;
+if(dialogDom !== null){
+    var dialog = new MDCDialog(dialogDom);
+
+    dialog.listen('MDCDialog:accept', function() {
+
+        window.location.href = document.querySelector('#remove-account-dialog-accept').value;
+    });
+
+    document.querySelector('#remove-account-dialog-button').addEventListener('click', function (evt) {
+        dialog.lastFocusedTarget = evt.target;
+        dialog.show();
+    });
+}
+
+var snackbarDom = document.querySelector('.mdc-snackbar');
+
+if(snackbarDom !== null){
+
+    const snackbar = new MDCSnackbar(document.querySelector('.mdc-snackbar'));
+
+    const dataObj = {
+        message: document.querySelector('.mdc-snackbar__message').innerHTML,
+    };
+
+    snackbar.show(dataObj);
+
+}
+
 
 import autoInit from '@material/auto-init';
 import {MDCTextField} from '@material/textfield';
@@ -28,4 +55,3 @@ autoInit();
 
 //Test JS
 console.log('hello world 2');
-
