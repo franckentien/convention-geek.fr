f69155d41bb19374bb2549acc6a23f68462e4506
franckentien <franckentien@hotmail.fr>
2018-08-28 19:28:40 +0200

    update webpackfile with import of MDC

--- webpack.config.js
+++ webpack.config.js
@@ -1,58 +1,41 @@
-var Encore = require('@symfony/webpack-encore');
+const autoprefixer = require('autoprefixer');
 
-Encore
-// directory where compiled assets will be stored
-    .setOutputPath('web/build/')
-    // public path used by the web server to access the output path
-    .setPublicPath('/build')
-    // only needed for CDN's or sub-directory deploy
-    //.setManifestKeyPrefix('build/')
-
-    /*
-     * ENTRY CONFIG
-     *
-     * Add 1 entry for each "page" of your app
-     * (including one that's included on every page - e.g. "app")
-     *
-     * Each entry will result in one JavaScript file (e.g. app.js)
-     * and one CSS file (e.g. app.css) if you JavaScript imports CSS.
-     */
-    .addEntry('app', './assets/js/app.js')
-    //.addEntry('page1', './assets/js/page1.js')
-    //.addEntry('page2', './assets/js/page2.js')
-
-    .cleanupOutputBeforeBuild()
-    .enableSourceMaps(!Encore.isProduction())
-    // enables hashed filenames (e.g. app.abc123.css)
-    .enableVersioning(Encore.isProduction())
-
-    .enablePostCssLoader((options) => {
-         options.config = {
-             path: 'app/config/postcss.config.js'
-         };
-     })
-
-    .configureBabel(function(babelConfig) {
-        // add additional presets
-        //babelConfig.presets.push('es2017');
-        babelConfig.presets.push('es2015');
-        // no plugins are added by default, but you can add some
-        // babelConfig.plugins.push('styled-jsx/babel');
-    })
-
-    // uncomment if you use TypeScript
-    //.enableTypeScriptLoader()
-
-    // uncomment if you use Sass/SCSS files
-    .enableSassLoader(function(options) {
-        // https://github.com/webpack-contrib/less-loader#examples
-        // http://lesscss.org/usage/#command-line-usage-options
-        // options.relativeUrls = false;
-        options.includePaths = [ './node_modules' ];
-    })
-
-    // uncomment if you're having problems with a jQuery plugin
-    //.autoProvidejQuery()
-;
-
-module.exports = Encore.getWebpackConfig();
+module.exports = {
+    entry: ['./assets/js/app.js'],
+    output: {
+        filename: 'web/build/app.js',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: 'web/build/app.css',
+                        },
+                    },
+                    {loader: 'extract-loader'},
+                    {loader: 'css-loader'},
+                    {loader: 'postcss-loader',
+                        options: {
+                            plugins: () => [autoprefixer()],
+                        },
+                    },
+                    {
+                        loader: 'sass-loader',
+                        options: {
+                            includePaths: ['./node_modules'],
+                        },
+                    }],
+            },
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                query: {
+                    presets: ['es2015'],
+                },
+            }],
+    },
+};
